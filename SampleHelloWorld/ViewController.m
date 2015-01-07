9c85dc0d028faa60ec740965271fe3ff9ea33529
ei17ringo <ei17ringo@gmail.com>
2015-01-07 20:38:53 +0800

    init commit

--- SampleHelloWorld/ViewController.m
+++ SampleHelloWorld/ViewController.m
@@ -17,6 +17,38 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.myLabel.text = @"Hello,Cebu!";
+    
+    //変数宣言と代入
+    NSString *str = @"Hello,Japan.";
+    
+    int number = 9;
+    
+//    number = number + 1;
+    number += 1;
+    
+    if (number < 10) {
+        number++;
+    }else{
+        number = 100;
+    }
+    
+    
+    switch (number) {
+        case 1:
+            NSLog(@"%dです",number);
+            break;
+        case 100:
+            NSLog(@"1より大きい%dです",number);
+            break;
+        default:
+            NSLog(@"1でも100でもない");
+            break;
+    }
+    
+    NSLog(@"%@と%d",str,number);
+    
 }
 
 - (void)didReceiveMemoryWarning {
